26cd1089214356a6c512ac75942f28f5d09c766d
Alissa Cohan <alissacohan18@gmail.com>
2023-03-30 17:48:16 -0400

    added resolvers & typeDefs schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    username: String
+    email: String
+  }
+
+  type Book {
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    user(username: String!): User
+    books(title: String): [Book]
+    books(bookId: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(bookId: String!): Book
+    removeBook(bookId: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
